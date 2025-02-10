cd70f9fa9ab5014aeb20c85e3d447d85ab42f2e8
Inam-ho <mmuhammadinam235@gmail.com>
2025-02-10 23:21:54 +0500

    base url chnage

--- utils/url_config.js
+++ utils/url_config.js
@@ -1,2 +1,3 @@
 // export const API_URL = "http://localhost:8000";
-export const API_URL = "https://demo-milkywayitsolution.com/";
+// export const API_URL = "https://demo-milkywayitsolution.com/";
+export const API_URL = "https://bttdc.nazmul.xyz/";
